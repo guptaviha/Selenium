46baaaded9332ba4f92a5ff884126e01d9c96d5d
Viha Gupta <vihagupta95@gmail.com>
2020-11-29 19:07:51 -0500

    log in to bublup with script

--- selenium_sample.py
+++ selenium_sample.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-driver.get("https://www.python.org")
+
+driver.get("https://mystuff.bublup.com/mybublup/#/mystuff/top/folder")
 print(driver.title)
-search_bar = driver.find_element_by_name("q")
-search_bar.clear()
-search_bar.send_keys("getting started with python")
-search_bar.send_keys(Keys.RETURN)
-print(driver.current_url)
+driver.implicitly_wait(6)
+email_bar = driver.find_element_by_id("email")
+email_bar.clear()
+email_bar.send_keys("vihagupta95@gmail.com")
+pass_bar = driver.find_element_by_id("password")
+pass_bar.clear()
+pass_bar.send_keys("HW191Dbub!")
+button = driver.find_element_by_id("sign-in-submit")
+button.click()
+
+#driver.close()
